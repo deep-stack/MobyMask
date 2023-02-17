dea9e2b2f6c420b92d9b2f8b6e18208def60c11c
Dhruv Srivastava <dhruvdhs.ds@gmail.com>
2023-02-17 18:05:51 +0530

    accept private key for contract deployment

--- packages/hardhat/scripts/deploy.js
+++ packages/hardhat/scripts/deploy.js
@@ -1,84 +1,19 @@
-/* eslint no-use-before-define: "warn" */
 const fs = require("fs");
-const chalk = require("chalk");
-const { config, ethers, tenderly, run } = require("hardhat");
-const { utils } = require("ethers");
-const R = require("ramda");
-
-/*
-
- _______ _________ _______  _______
-(  ____ \\__   __/(  ___  )(  ____ )
-| (    \/   ) (   | (   ) || (    )|
-| (_____    | |   | |   | || (____)|
-(_____  )   | |   | |   | ||  _____)
-      ) |   | |   | |   | || (
-/\____) |   | |   | (___) || )
-\_______)   )_(   (_______)|/
-
-This deploy script is no longer in use, but is left for reference purposes!
-
-scaffold-eth now uses hardhat-deploy to manage deployments, see the /deploy folder
-And learn more here: https://www.npmjs.com/package/hardhat-deploy
-
-*/
+const { ethers, utils, ContractFactory } = require("ethers");
+const secp256k1 = require("secp256k1");
+const chalk  = require("chalk");
+const should = require("chai").should();
 
 const main = async () => {
-  console.log("\n\n 📡 Deploying...\n");
+  // normalize args as an array
+  var args = process.argv.slice(2);
+  var privKey = args[0];
 
   await deploy("PhisherRegistry", {
-    // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
-    from: deployer,
+    privKey: privKey,
     args: [ "MobyMask" ],
     log: true,
   });
-  // use for local token bridging
-  // const mockToken = await deploy("MockERC20") // <-- add in constructor args like line 19 vvvv
-
-  //const yourContract = await ethers.getContractAt('YourContract', "0xaAC799eC2d00C013f1F11c37E654e59B0429DF6A") //<-- if you want to instantiate a version of a contract at a specific address!
-  //const secondContract = await deploy("SecondContract")
-
-  // const exampleToken = await deploy("ExampleToken")
-  // const examplePriceOracle = await deploy("ExamplePriceOracle")
-  // const smartContractWallet = await deploy("SmartContractWallet",[exampleToken.address,examplePriceOracle.address])
-
-  /*
-  //If you want to send value to an address from the deployer
-  const deployerWallet = ethers.provider.getSigner()
-  await deployerWallet.sendTransaction({
-    to: "0x34aA3F359A9D614239015126635CE7732c18fDF3",
-    value: ethers.utils.parseEther("0.001")
-  })
-  */
-
-  /*
-  //If you want to send some ETH to a contract on deploy (make your constructor payable!)
-  const yourContract = await deploy("YourContract", [], {
-  value: ethers.utils.parseEther("0.05")
-  });
-  */
-
-  /*
-  //If you want to link a library into your contract:
-  // reference: https://github.com/austintgriffith/scaffold-eth/blob/using-libraries-example/packages/hardhat/scripts/deploy.js#L19
-  const yourContract = await deploy("YourContract", [], {}, {
-   LibraryName: **LibraryAddress**
-  });
-  */
-
-  //If you want to verify your contract on tenderly.co (see setup details in the scaffold-eth README!)
-  /*
-  await tenderlyVerify(
-    {contractName: "YourContract",
-     contractAddress: yourContract.address
-  })
-  */
-
-  console.log(
-    " 💾  Artifacts (address, abi, and args) saved to: ",
-    chalk.blue("packages/hardhat/artifacts/"),
-    "\n\n"
-  );
 };
 
 const deploy = async (
@@ -87,127 +22,49 @@ const deploy = async (
   overrides = {},
   libraries = {}
 ) => {
-  console.log(` 🛰  Deploying: ${contractName}`);
+    const privateKey = _args.privKey;
+    should.exist(privateKey, "Private key cannot be empty.");
+    
+    const phisherRegistryArtifacts = require('../artifacts/contracts/PhisherRegistry.sol/PhisherRegistry.json');
+        
+    const contractAbi = phisherRegistryArtifacts.abi;
+    const contractBytecode = phisherRegistryArtifacts.bytecode;
+    const factory = new ContractFactory(contractAbi, contractBytecode);
 
-  const contractArgs = _args || [];
-  const contractArtifacts = await ethers.getContractFactory(contractName, {
-    libraries: libraries,
-  });
-  const deployed = await contractArtifacts.deploy(...contractArgs, overrides);
-  const encoded = abiEncodeArgs(deployed, contractArgs);
-  fs.writeFileSync(`artifacts/${contractName}.address`, deployed.address);
+    const privateKey_hex = parsePrivateKey(privateKey);
+    const ethAddress = ethAddressFromPrivateKey(privateKey_hex);
 
-  let extraGasInfo = "";
-  if (deployed && deployed.deployTransaction) {
-    const gasUsed = deployed.deployTransaction.gasLimit.mul(
-      deployed.deployTransaction.gasPrice
+    const jsonRpcProvider = new ethers.providers.JsonRpcProvider(); 
+    const deployer = await jsonRpcProvider.getSigner(ethAddress);
+    const contract = await factory.connect(deployer).deploy(_args);        
+
+    console.log(
+        chalk.cyan(contractName),
+        "deployed to:",
+        chalk.magenta(contract.address)
     );
-    extraGasInfo = `${utils.formatEther(gasUsed)} ETH, tx hash ${
-      deployed.deployTransaction.hash
-    }`;
-  }
 
-  console.log(
-    " 📄",
-    chalk.cyan(contractName),
-    "deployed to:",
-    chalk.magenta(deployed.address)
-  );
-  console.log(" ⛽", chalk.grey(extraGasInfo));
-
-  await tenderly.persistArtifacts({
-    name: contractName,
-    address: deployed.address,
-  });
-
-  if (!encoded || encoded.length <= 2) return deployed;
-  fs.writeFileSync(`artifacts/${contractName}.args`, encoded.slice(2));
-
-  return deployed;
+    return contract;
 };
 
 // ------ utils -------
-
-// abi encodes contract arguments
-// useful when you want to manually verify the contracts
-// for example, on Etherscan
-const abiEncodeArgs = (deployed, contractArgs) => {
-  // not writing abi encoded args if this does not pass
-  if (
-    !contractArgs ||
-    !deployed ||
-    !R.hasPath(["interface", "deploy"], deployed)
-  ) {
-    return "";
-  }
-  const encoded = utils.defaultAbiCoder.encode(
-    deployed.interface.deploy.inputs,
-    contractArgs
-  );
-  return encoded;
+// parses recieved private key from string to uint8Array
+const parsePrivateKey = (PrivateKey) => {
+    var result = [];
+    for(var i = 0; i < PrivateKey.length; i+=2)
+    {
+        result.push(parseInt(PrivateKey.substring(i, i + 2), 16));
+    }
+    
+    return Uint8Array.from(result);
 };
 
-// checks if it is a Solidity file
-const isSolidity = (fileName) =>
-  fileName.indexOf(".sol") >= 0 &&
-  fileName.indexOf(".swp") < 0 &&
-  fileName.indexOf(".swap") < 0;
+// generates ethereum address from account's private key
+const ethAddressFromPrivateKey = (privateKey) => {
+    const publicKey = secp256k1.publicKeyCreate(privateKey);
+    const ethAddress = utils.computeAddress(publicKey);
 
-const readArgsFile = (contractName) => {
-  let args = [];
-  try {
-    const argsFile = `./contracts/${contractName}.args`;
-    if (!fs.existsSync(argsFile)) return args;
-    args = JSON.parse(fs.readFileSync(argsFile));
-  } catch (e) {
-    console.log(e);
-  }
-  return args;
-};
-
-function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
-// If you want to verify on https://tenderly.co/
-const tenderlyVerify = async ({ contractName, contractAddress }) => {
-  let tenderlyNetworks = [
-    "kovan",
-    "goerli",
-    "mainnet",
-    "rinkeby",
-    "ropsten",
-    "matic",
-    "mumbai",
-    "xDai",
-    "POA",
-  ];
-  let targetNetwork = process.env.HARDHAT_NETWORK || config.defaultNetwork;
-
-  if (tenderlyNetworks.includes(targetNetwork)) {
-    console.log(
-      chalk.blue(
-        ` 📁 Attempting tenderly verification of ${contractName} on ${targetNetwork}`
-      )
-    );
-
-    await tenderly.persistArtifacts({
-      name: contractName,
-      address: contractAddress,
-    });
-
-    let verification = await tenderly.verify({
-      name: contractName,
-      address: contractAddress,
-      network: targetNetwork,
-    });
-
-    return verification;
-  } else {
-    console.log(
-      chalk.grey(` 🧐 Contract verification not supported on ${targetNetwork}`)
-    );
-  }
+    return ethAddress;
 };
 
 main()
